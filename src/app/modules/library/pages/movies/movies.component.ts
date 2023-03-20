43be5f21f213c4d781f4fb752a22ebf1ef442afa
Igor Ferraz <igor.ferraz@outlook.com>
2023-03-19 23:52:10 -0300

    feat(movies): carousel arrows fixes

--- src/app/modules/library/pages/movies/movies.component.ts
+++ src/app/modules/library/pages/movies/movies.component.ts
@@ -71,6 +71,62 @@ export class MoviesComponent {
                         duration: 200
                     }
                 ]
+            },
+            {
+                id: '2',
+                name: 'Mais assistidos',
+                movies: [
+                    {
+                        id: '1',
+                        title: 'O Senhor dos Anés: A Sociedade do Anel',
+                        duration: 200
+                    },
+                    {
+                        id: '2',
+                        title: 'Harry Potter e a Pedra Filosofal',
+                        duration: 200
+                    },
+                    {
+                        id: '3',
+                        title: 'John Wick 4: Baba Yaga',
+                        duration: 200
+                    },
+                    {
+                        id: '4',
+                        title: 'Forrest Gump',
+                        duration: 200
+                    },
+                    {
+                        id: '5',
+                        title: 'O Brilho Eterno de uma Mente sem Lembranças',
+                        duration: 200
+                    },
+                    {
+                        id: '6',
+                        title: 'Era uma Vez em Hollywood',
+                        duration: 200
+                    },
+                    {
+                        id: '7',
+                        title: 'Os Sete Odiados',
+                        duration: 200
+                    },
+                    {
+                        id: '8',
+                        title: '1917',
+                        duration: 200
+                    },
+                    {
+                        id: '9',
+                        title: 'O Virgem de 40 Anos',
+                        duration: 200
+                    },
+                    {
+                        id: '10',
+                        title: 'O Todo Poderoso',
+                        duration: 200
+                    }
+                ]
             });
     }
 
