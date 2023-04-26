0b2a77994cd83ce0d1be0a4d39255637adcc1c4c
Igor Ferraz <igor.ferraz@outlook.com>
2023-04-25 23:52:53 -0300

    feat(responsividade): início de padronização

--- src/app/features/auth/pages/forgotten-password/forgotten-password.component.ts
+++ src/app/features/auth/pages/forgotten-password/forgotten-password.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+import { Breakpoint } from 'src/app/shared/models/responsive/breakpoint.enum';
 
 @Component({
-  selector: 'app-forgotten-password',
-  templateUrl: './forgotten-password.component.html',
-  styleUrls: ['./forgotten-password.component.scss']
+    selector: 'app-forgotten-password',
+    templateUrl: './forgotten-password.component.html',
+    styleUrls: ['./forgotten-password.component.scss']
 })
-export class ForgottenPasswordComponent {
+export class ForgottenPasswordComponent implements OnInit {
+    public breakpoint = Breakpoint;
+    public size = Breakpoint.None;
 
+    constructor(private responsive: BreakpointObserver) { }
+
+    ngOnInit(): void {
+        this.responsive.observe([
+            Breakpoints.Medium,
+            Breakpoints.Large
+        ]).subscribe(result => {
+            const breakpoints = result.breakpoints;
+
+            this.size = this.breakpoint.Small;
+
+            if (breakpoints[Breakpoints.Medium]) {
+                this.size = this.breakpoint.Medium;
+            }
+            else if (breakpoints[Breakpoints.Large]) {
+                this.size = this.breakpoint.Large;
+            }
+        });
+    }
 }
--- src/app/features/auth/pages/sign-in/sign-in.component.ts
+++ src/app/features/auth/pages/sign-in/sign-in.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+import { Breakpoint } from 'src/app/shared/models/responsive/breakpoint.enum';
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+    selector: 'app-sign-in',
+    templateUrl: './sign-in.component.html',
+    styleUrls: ['./sign-in.component.scss']
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
+    public breakpoint = Breakpoint;
+    public size = Breakpoint.None;
 
+    constructor(private responsive: BreakpointObserver) { }
+
+    ngOnInit(): void {
+        this.responsive.observe([
+            Breakpoints.Medium,
+            Breakpoints.Large
+        ]).subscribe(result => {
+            const breakpoints = result.breakpoints;
+
+            this.size = this.breakpoint.Small;
+
+            if (breakpoints[Breakpoints.Medium]) {
+                this.size = this.breakpoint.Medium;
+            }
+            else if (breakpoints[Breakpoints.Large]) {
+                this.size = this.breakpoint.Large;
+            }
+        });
+    }
 }
