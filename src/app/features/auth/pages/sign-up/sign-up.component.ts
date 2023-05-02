6b75a7ddee7728c194ea7fc5b306b8629b33f984
Igor Ferraz <igor.ferraz@outlook.com>
2023-05-02 18:07:29 -0300

    fix(landing): responsive

--- src/app/features/auth/pages/sign-up/sign-up.component.ts
+++ src/app/features/auth/pages/sign-up/sign-up.component.ts
@@ -1,17 +1,39 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Breakpoint } from 'src/app/shared/models/responsive/breakpoint.enum';
 
 @Component({
-  selector: 'app-sign-up',
-  templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+    selector: 'app-sign-up',
+    templateUrl: './sign-up.component.html',
+    styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+    public breakpoint = Breakpoint;
+    public size = Breakpoint.None;
 
-  constructor(private router: Router) { }
+    constructor(private router: Router, private responsive: BreakpointObserver) { }
 
-  public back() {
-    // Ask for sure if form is dirty
-    this.router.navigateByUrl("/auth/sign-in");
-  }
+    ngOnInit(): void {
+        this.responsive.observe([
+            Breakpoints.Medium,
+            Breakpoints.Large
+        ]).subscribe(result => {
+            const breakpoints = result.breakpoints;
+
+            this.size = this.breakpoint.Small;
+
+            if (breakpoints[Breakpoints.Medium]) {
+                this.size = this.breakpoint.Medium;
+            }
+            else if (breakpoints[Breakpoints.Large]) {
+                this.size = this.breakpoint.Large;
+            }
+        });
+    }
+
+    public back() {
+        // Ask for sure if form is dirty
+        this.router.navigateByUrl("/auth/sign-in");
+    }
 }
--- src/app/features/landing/pages/landing/landing.component.ts
+++ src/app/features/landing/pages/landing/landing.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+import { Breakpoint } from 'src/app/shared/models/responsive/breakpoint.enum';
 
 @Component({
-  selector: 'app-landing',
-  templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+    selector: 'app-landing',
+    templateUrl: './landing.component.html',
+    styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
+    public breakpoint = Breakpoint;
+    public size = Breakpoint.None;
 
+    constructor(private responsive: BreakpointObserver) { }
+
+    ngOnInit(): void {
+        this.responsive.observe([
+            Breakpoints.Medium,
+            Breakpoints.Large
+        ]).subscribe(result => {
+            const breakpoints = result.breakpoints;
+
+            this.size = this.breakpoint.Small;
+
+            if (breakpoints[Breakpoints.Medium]) {
+                this.size = this.breakpoint.Medium;
+            }
+            else if (breakpoints[Breakpoints.Large]) {
+                this.size = this.breakpoint.Large;
+            }
+        });
+    }
 }
